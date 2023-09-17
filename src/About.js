eb804b4c6c16aca6d12f733175c1d4328523642a
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-09-18 00:04:22 +0530

    Update About.js
    Syntax of About.js

--- src/About.js
+++ src/About.js
@@ -1,1 +1,26 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about" className="about">
+      <div className="container">
+        <h2>About the Temple Renovation</h2>
+        <p>
+          Our beloved temple is in need of renovation to preserve its rich
+          heritage and ensure its continued use for generations to come. The
+          renovation project aims to restore the temple's architectural beauty
+          and enhance its facilities to better serve our community.
+        </p>
+        <p>
+          Your generous donations will play a crucial role in making this
+          project a reality. Together, we can help preserve our cultural
+          heritage and create a place of worship that future generations can
+          cherish.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
 
