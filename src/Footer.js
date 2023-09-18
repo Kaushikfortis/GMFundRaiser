514fe811de0de0a773f1d221345316a0b526d0cb
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-09-18 21:12:37 +0530

    Update Footer.js
    Footer Syntax added.

--- src/Footer.js
+++ src/Footer.js
@@ -1,1 +1,27 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-copyright">
+          <p>&copy; 2023 Temple Renovation Fundraiser</p>
+        </div>
+        <div className="footer-social">
+          <a href="#" className="social-link">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" className="social-link">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" className="social-link">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
 
