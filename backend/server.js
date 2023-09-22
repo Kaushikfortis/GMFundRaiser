75c8ccd67d273f81f0611c6407bc532382611287
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-09-22 12:07:46 +0530

    Update server.js
    Added some features and updated DB cluster url.

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,62 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+// server.js
 
-// Connect to MongoDB (replace 'your-database-url' with your actual MongoDB connection URL)
-mongoose.connect('postgresql://postgres:GMFundraiser123#@db.jnzjpwilnfcwwxgilosz.supabase.co:5432/postgres', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error);
-  });
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001; // You can change the port number
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// Define routes for handling donations
-// Create, Read, Update, Delete (CRUD) operations for donations go here
+// Sample data structure for storing donor information
+const donors = [];
 
-// Start the server on a specified port (e.g., 5000)
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Define an endpoint to create a new donor record
+app.post('/api/donors', (req, res) => {
+  const { name, amount } = req.body;
+
+  // Validate the request data
+  if (!name || !amount || isNaN(amount)) {
+    return res.status(400).json({ error: 'Invalid data. Name and amount are required.' });
+  }
+
+  // Create a new donor object
+  const newDonor = {
+    id: donors.length + 1, // You can use a more robust ID generation method
+    name,
+    amount: parseFloat(amount), // Convert amount to a number
+  };
+
+  // Add the donor to the data structure
+  donors.push(newDonor);
+
+  return res.status(201).json(newDonor);
 });
+
+// Define an endpoint to retrieve all donors
+app.get('/api/donors', (req, res) => {
+  return res.status(200).json(donors);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+const mongoose = require('mongoose');
+
+// Replace YOUR_MONGODB_URI with your actual MongoDB URI
+const mongoURI = 'mongodb+srv://kaushik:Pass@123@donors.0v5hnbm.mongodb.net/?retryWrites=true&w=majority';
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
