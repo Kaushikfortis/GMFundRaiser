cb0406e0f390234d19c712b2d8a4f043f42b50cf
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-09-22 13:03:51 +0530

    Update DonorList.js
    Added some features

--- src/DonorList.js
+++ src/DonorList.js
@@ -1,1 +1,19 @@
+import React from 'react';
+
+function DonorList({ donors }) {
+  return (
+    <div className="donor-list">
+      <h2>Donor List</h2>
+      <ul>
+        {donors.map((donor, index) => (
+          <li key={index}>
+            {donor.name}: ${donor.amount.toFixed(2)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DonorList;
 
