71a62b0ebed254dfce372b1a86a246fac58929cc
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-09-22 12:40:27 +0530

    Update App.js
    Added some features

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 import React, { useState } from 'react';
 import './App.css';
 
@@ -6,15 +8,27 @@ import DonationForm from './DonationForm';
 import About from './About';
 import Contact from './Contact';
 import Footer from './Footer';
+import DonorList from './DonorList';
+
+
+
 
 function App() {
   const [totalDonations, setTotalDonations] = useState(0);
+  const [donors, setDonors] = useState([]);
 
-  const handleDonation = (amount) => {
-    // Implement donation processing logic here (e.g., sending data to a server).
-    // For simplicity, we'll just update the total donations for this example.
-    setTotalDonations((prevTotal) => prevTotal + amount);
+  //const handleDonation = ({ amount, name }) => {
+    //setTotalDonations((prevTotal) => prevTotal + amount);
+    //setDonors((prevDonors) => [...prevDonors, { name, amount }]);
+  //};
+  
+
+  const handleDonation = ({ amount, name }) => {
+    const amountAsNumber = parseFloat(amount);
+    setTotalDonations((prevTotal) => prevTotal + amountAsNumber);
+    setDonors((prevDonors) => [...prevDonors, { name, amount }]);
   };
+  
 
   return (
     <div className="App">
@@ -27,16 +41,17 @@ function App() {
         </p>
         <DonationForm onDonation={handleDonation} />
         <div className="total-donations">
-        <p>Total Donations: ${totalDonations.toFixed(2)}</p>
-      </div>
+          <p>Total Donations: ${totalDonations.toFixed(2)}</p>
+        </div>
+        <DonorList donors={donors} />
         <About />
         <Contact />
       </main>
       <Footer />
-      
     </div>
   );
 }
 
 export default App;
+
 
